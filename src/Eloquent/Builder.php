3ef12df3a663d7de1ddbbeb51509eef38952e68e
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2024-09-29 13:48:02 +0200

    Increase PHPStan level

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Builder as Base;
 use Staudenmeir\EloquentParamLimitFix\Traits\BuildsParamLimitFixQueries;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\Traits\BuildsAdjacencyListQueries;
 
+/**
+ * @template TModel of \Illuminate\Database\Eloquent\Model
+ *
+ * @extends \Illuminate\Database\Eloquent\Builder<TModel>
+ */
 class Builder extends Base
 {
     use BuildsAdjacencyListQueries;
