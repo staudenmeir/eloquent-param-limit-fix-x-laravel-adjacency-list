393d417ccab2530b44d40f7a6a47d94035e9318e
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2025-03-22 09:58:19 +0100

    Use PHP 8.4 by default

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -2,9 +2,8 @@
 
 namespace Staudenmeir\EloquentParamLimitFixXLaravelAdjacencyList\Eloquent;
 
-use Illuminate\Database\Eloquent\Builder as Base;
 use Staudenmeir\EloquentParamLimitFix\Traits\BuildsParamLimitFixQueries;
-use Staudenmeir\LaravelAdjacencyList\Eloquent\Traits\BuildsAdjacencyListQueries;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\Builder as Base;
 
 /**
  * @template TModel of \Illuminate\Database\Eloquent\Model
@@ -13,6 +12,5 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\Traits\BuildsAdjacencyListQueries;
  */
 class Builder extends Base
 {
-    use BuildsAdjacencyListQueries;
     use BuildsParamLimitFixQueries;
 }
